547d893630fbb48e9803d860a2e923115a9f2475
SulemanAwais <sulemanawais2001@gmail.com>
2024-08-27 16:28:42 +0500

    Update settings.py

--- basic_project/basic_project/settings.py
+++ basic_project/basic_project/settings.py
@@ -29,8 +29,6 @@ ALLOWED_HOSTS = ['SulemanAwais0.pythonanywhere.com', '127.0.0.1']
 
 import os
 
-# Add this line to the end of the file
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,7 +117,9 @@ USE_TZ = True
 # static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
